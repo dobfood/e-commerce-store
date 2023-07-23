49283e1c4ab58e65af9a0eb705c6d515a788a9b6
anhkiet <kiet47579@gmail.com>
2023-07-23 13:11:56 +0700

    change rou

--- components/ui/icon-button.tsx
+++ components/ui/icon-button.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { MouseEventHandler } from "react";
-import { cn } from "../lib/utils";
+import { cn } from "@/libs/utils";
 
 interface IconButtonProps {
   onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
